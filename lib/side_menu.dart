04945c456f47c11bf142671531866a2a7ecb8705
ctnadovich <github@nadovich.com>
2025-01-08 18:38:55 -0500

    Copyright year

--- lib/side_menu.dart
+++ lib/side_menu.dart
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with eBrevet.  If not, see <http://www.gnu.org/licenses/>.
 
-import 'package:ebrevet_card/mylogger.dart';
+// import 'package:ebrevet_card/mylogger.dart';
 import 'package:ebrevet_card/past_events_page.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -106,8 +106,6 @@ class SideMenuDrawer extends StatelessWidget {
   void aboutDialog(BuildContext context) {
     var cYear = DateTime.now().year;
     showAboutDialog(
-        applicationLegalese:
-            '(c)$cYear Chris Nadovich. This is free software licensed under GPLv3.',
         context: context,
         applicationName: 'eBrevet',
         applicationIcon: Image.asset(
@@ -116,6 +114,8 @@ class SideMenuDrawer extends StatelessWidget {
         ),
         applicationVersion:
             "v${AppSettings.version ?? '?'} (${AppSettings.buildNumber})",
+        applicationLegalese:
+            '(c)$cYear Chris Nadovich. This is free software licensed under GPLv3.',
         children: [
           const SizedBox(
             height: 16,
